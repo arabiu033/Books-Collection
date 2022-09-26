48c6038ea9d3c548a50b42cf1bb729b9180d51d8
Aminu Rabiu <arabiu033@gmail.com>
2022-09-26 17:20:04 +0100

    Import the required modules in index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import { listBooks, books, bookAuthor, bookTitle, addBtn } from "./modules/variables.js";
+import './modules/interface.js'
+
+export function updateBtns() {
+  listBooks.querySelectorAll('button').forEach((element) => {
+    element.addEventListener('click', () => books.removeBooks(element));
+  });
+}
+
+addBtn.addEventListener('click', () => {
+  books.setBookCount();
+  if (!bookTitle.value || !bookAuthor.value) {
+    return;
+  }
+
+  books.add(bookTitle.value, bookAuthor.value);
+  books.cBooks.push({ id: books.getBookCount(), title: bookTitle.value, author: bookAuthor.value });
+  localStorage.setItem('collectionOfBooks', JSON.stringify(books.cBooks));
+  updateBtns();
+  bookAuthor.value = '';
+  bookTitle.value = '';
+});
